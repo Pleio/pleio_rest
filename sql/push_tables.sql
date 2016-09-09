526d5f48449508a83cfded781eac1e554afc3864
Bart Jeukendrup <bart@jeukendrup.nl>
2016-09-09 15:01:01 +0200

    made schema compatible with MariaDB

--- sql/push_tables.sql
+++ sql/push_tables.sql
@@ -1,7 +1,20 @@
-CREATE TABLE IF NOT EXISTS elgg_push_notifications_count (id bigint(20) NOT NULL, user_guid bigint(20) NOT NULL, site_guid bigint(20) NOT NULL, container_guid bigint(20) NOT NULL, count int(11) NOT NULL);
-ALTER TABLE `elgg_push_notifications_count` ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `user_guid` (`user_guid`,`container_guid`);
-ALTER TABLE `elgg_push_notifications_count` MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+CREATE TABLE IF NOT EXISTS elgg_push_notifications_count (
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `user_guid` bigint(20) NOT NULL,
+    `site_guid` bigint(20) NOT NULL,
+    `container_guid` bigint(20) NOT NULL,
+    `count` int(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `user_guid` (`user_guid`,`container_guid`)
+);
 
-CREATE TABLE IF NOT EXISTS elgg_push_notifications_subscriptions (id bigint(20) NOT NULL, user_guid bigint(20) NOT NULL, client_id varchar(100) NOT NULL, service enum('gcm','apns', 'wns') NOT NULL, device_id varchar(100) NOT NULL, token varchar(100) NOT NULL);
-ALTER TABLE `elgg_push_notifications_subscriptions` ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `token` (`service`, `token`);
-ALTER TABLE `elgg_push_notifications_subscriptions` MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+CREATE TABLE IF NOT EXISTS elgg_push_notifications_subscriptions (
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `user_guid` bigint(20) NOT NULL,
+    `client_id` varchar(100) NOT NULL,
+    `service` enum('gcm','apns', 'wns') NOT NULL,
+    `device_id` varchar(100) NOT NULL,
+    `token` varchar(100) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `token` (`service`, `token`)
+);
